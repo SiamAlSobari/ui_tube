ce91288e238f12ac8253ef086d88097d8797f8c0
YammD123 <siamqueen52@gmail.com>
2025-07-28 21:33:07 +0700

    feat(layout): integrate Svelte Query and Devtools for improved data fetching

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import '../app.css';
-
+	import { browser } from '$app/environment';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
 	let { children } = $props();
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
 </script>
 
-{@render children()}
+<QueryClientProvider client={queryClient}>
+	{@render children()}
+	<SvelteQueryDevtools />
+</QueryClientProvider>
