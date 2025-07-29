885667679f9d4e58ce781bc197e9c34e5532f336
YammD123 <siamqueen52@gmail.com>
2025-07-29 09:12:44 +0700

    feat(): add theme toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
-	import Button from "$lib/components/ui/button/button.svelte";
-
-
+	import { Button } from '$lib/components/ui/button';
+	import ThemeTogle from '../common/ui/theme-togle.svelte';
 </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>saassasa</p>
-<Button >Click Me</Button>
+<Button>Click Me</Button>
+<ThemeTogle />
+<p>To learn more about Svelte, visit <a href="https://svelte.dev">svelte.dev</a></p>
+<p>To learn more about SvelteKit, visit <a href="https://kit.svelte.dev">kit.svelte.dev</a></p>
